3e520fb9802f9968a6b80ea40f00afb5cad3b39d
vicanso <vicansocanbico@gmail.com>
2019-02-24 20:21:58 +0800

    feat: add error handler middleware

---
+++ error_handler_test.go
@@ -0,0 +1,32 @@
+package errorhandler
+
+import (
+	"errors"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/vicanso/cod"
+)
+
+func TestErrorHandler(t *testing.T) {
+	fn := NewDefault()
+	req := httptest.NewRequest("GET", "/users/me", nil)
+	resp := httptest.NewRecorder()
+	c := cod.NewContext(resp, req)
+	c.Next = func() error {
+		return errors.New("abcd")
+	}
+	c.CacheMaxAge("5m")
+	err := fn(c)
+	if err != nil {
+		t.Fatalf("error handler fail, %v", err)
+	}
+	if c.GetHeader("Cache-Control") != "no-cache" {
+		t.Fatalf("error response should be no cache")
+	}
+	ct := c.GetHeader(cod.HeaderContentType)
+	if c.BodyBuffer.String() != `{"statusCode":500,"category":"cod-error-handler","message":"abcd"}` ||
+		ct != "application/json; charset=UTF-8" {
+		t.Fatalf("error handler fail")
+	}
+}
